662f3a75f6c728587b4e7694c4e5b35f012a07bb
panosdou <pndoumas@gmail.com>
2023-12-23 01:16:47 +0200

    added decompression testing

--- compression.py
+++ compression.py
@@ -2,25 +2,22 @@ import zlib
 
 def calculate_compression_ratio(data):
 
-    data_bytes = ''.join(data).encode()
-    #data_bytes = ''.join(format(ord(x), 'b') for x in data).encode()
-    #print(data_bytes)
-    compress = zlib.compressobj(zlib.Z_DEFAULT_COMPRESSION, zlib.DEFLATED, -15)
-    compressed_data = compress.compress(data_bytes)
-    compressed_data += compress.flush()
-    #print(compressed_data)
-    #print(len(compressed_data))
-    #print(type(data_bytes))
-    #compressed_data = zlib.compress(data_bytes, level = 6)
-    #print(len(data_bytes))
+    data_bytes = ''.join(data).encode('utf-8')
+    # compress = zlib.compressobj(zlib.Z_DEFAULT_COMPRESSION, zlib.DEFLATED, -15)
+    # compressed_data = compress.compress(data_bytes)
+    # compressed_data += compress.flush()
 
-    compression_ratio = len(data_bytes) / len(compressed_data)
+    compressed_data = zlib.compress(data_bytes, level = 6)
+    print(len(compressed_data))
+    #print(compressed_data)
+    print(len(data_bytes))
+    file = open('compressed.bin', 'wb')
+    file.write(compressed_data)
+    file.close()
+
+    compression_ratio =   len(data_bytes) / len(compressed_data)  
 
     return compression_ratio
-
-
-#data_letters = ["hello this is a test a little longer than this test would be if i did not write more than this but that's ok and I'm trying to figure out what's wrong with this can you help? Are you sure?"]
-#data_numbers = [1, 2, 3, 4, 5, 6, 7, 8]
 
 with open('text.txt', 'r') as file:
     data = file.read()
@@ -31,3 +28,13 @@ compression_ratio = calculate_compression_ratio(data)
 
 
 print(compression_ratio)
+
+#### test for decompression ####
+
+# with open('compressed.bin', 'rb') as file:
+#     compressed = file.read()
+#     original = zlib.decompress(compressed)
+#     message = original.decode('utf-8', errors='ignore')
+#     file.close()
+
+# print(message)
